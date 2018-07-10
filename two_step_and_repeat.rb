589a41a530f69a813f6179be36c41ce4fe2b3cd3
Hunter Sapienza <sapienzahunter@gmail.com>
2018-07-10 22:20:43 +0000

    automatically backed up by learn

--- two_step_and_repeat.rb
+++ two_step_and_repeat.rb
@@ -24,6 +24,15 @@ end
 
 def how_many_steps?
   steps = 0
+  loop do
+    steps += 1
+    puts steps
+    if steps%2 == 0
+      puts "Left"
+    else
+      puts "Right"
+    end
+    sleep(0.5)
   # Write a loop that fulfills all criteria from Part 3 of the README.md
 end
 
